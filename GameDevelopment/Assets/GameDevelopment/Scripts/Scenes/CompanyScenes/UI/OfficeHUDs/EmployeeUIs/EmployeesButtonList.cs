6e81c2d4e235612e22ab86a33f01ec6e7a1419b6
yamashiro_mondo <yamashiro@prolead.co.jp>
2021-02-02 19:11:33 +0900

    <修正>　社員UI修正

---
+++ GameDevelopment/Assets/GameDevelopment/Scripts/Scenes/CompanyScenes/UI/OfficeHUDs/EmployeeUIs/EmployeesButtonList.cs
@@ -0,0 +1,35 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameDevelopment.Scenes.CompanyScenes.UI.OfficeHUDs.EmployeeUIs
+{
+    /// <summary>
+    /// 社員UIのボタンリスト
+    /// </summary>
+    public class EmployeesButtonList : NewBehaviour
+    {
+        /// <summary>
+        /// 社員UI
+        /// </summary>
+        [SerializeField]
+        private EmployeesUI _employeesUI = default;
+
+        /// <summary>
+        /// 戻るボタン
+        /// </summary>
+        [SerializeField]
+        private Button _returnButton = default;
+
+        /// <summary>
+        /// Start
+        /// </summary>
+        private void Start()
+        {
+            //_returnButton
+            //    .OnClickAsObservable()
+            //    .Subscribe(_ => )
+                
+        }
+    }
+}
